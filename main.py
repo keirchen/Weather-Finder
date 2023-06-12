08d74ad92e048f890ce1af24d6dd5ba13ec93e3a
keirchen <133700694+keirchen@users.noreply.github.com>
2023-06-12 07:33:24 +0000

    print

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import urllib.request
 import json
 
-APIKey = "714c2da2279a72ec8bb713798409d352"
+apikey = "714c2da2279a72ec8bb713798409d352"
 
-cityName = input("City name: ")
-cityName = cityName.lower()
-url = "https://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s" % (cityName, APIKey)
+city_name = input("Search for a city by name: ")
+url = "https://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s" % (city_name, apikey)
 
 req = urllib.request.urlopen(url).read().decode()
 data = json.loads(req)
+
+print("City: %s\nWeather: %s\nTemperature: %s°F" % (data["name"], data["weather"][0]["main"], round((data["main"]["temp"] - 273.15) * 9/5 + 32, 2)))
