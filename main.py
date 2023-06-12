14acb5733b30943510b6010ab0fd1a33a88d4e51
keirchen <133700694+keirchen@users.noreply.github.com>
2023-06-12 07:28:52 +0000

    load data

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ APIKey = "714c2da2279a72ec8bb713798409d352"
 cityName = input("City name: ")
 cityName = cityName.lower()
 url = "https://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s" % (cityName, APIKey)
+
+req = urllib.request.urlopen(url).read().decode()
+data = json.loads(req)
